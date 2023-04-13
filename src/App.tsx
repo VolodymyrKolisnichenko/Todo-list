f3d511215988a46419d3b47b79a4d069d5de4632
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-13 23:22:29 +0200

    Update App.tsx
    created taskForTodoList for "completed" and "active"

--- src/App.tsx
+++ src/App.tsx
@@ -4,26 +4,35 @@ import "./App.css";
 import { TodoList } from "./Todolist";
 
 function App() {
-  let initTasks = [
-    {id: 1, title: "CSS&HTML", isDone: true},
-    {id: 2, title: "JS", isDone: true},
-    {id: 3, title: "React", isDone: false},
-    {id: 4, title: "Redux", isDone: false},
-  ]
 
-  let [tasks, setTasks] = useState(initTasks);
   // let [tasks, setTasks] = arr;
+  let [tasks, setTasks] = useState([
+    { id: 1, title: "CSS&HTML", isDone: true },
+    { id: 2, title: "JS", isDone: true },
+    { id: 3, title: "React", isDone: false },
+    { id: 4, title: "Redux", isDone: false },
+  ]);
+  let [filtered, stFilter] = useState("completed");
 
+  function removeTask(id: number) {
+    let filteredTasks = tasks.filter((t) => t.id !== id);
+    setTasks(filteredTasks);
+  }
 
-
-  function removeTask (id: number) {
-    let filteredTasks = tasks.filter(t => t.id !== id)
-    setTasks(filteredTasks)
+  let taskForTodoList = tasks;
+  if (filtered === "completed") {
+    taskForTodoList = tasks.filter(el => el.isDone === true)
+  }
+  if (filtered === "active") {
+    taskForTodoList = tasks.filter(el => el.isDone === false)
   }
 
   return (
     <div className="App">
-      <TodoList title="What to learn" tasks={tasks} removeTask={removeTask}/>
+      <TodoList 
+      title="What to learn" 
+      tasks={taskForTodoList} 
+      removeTask={removeTask} />
     </div>
   );
 }
