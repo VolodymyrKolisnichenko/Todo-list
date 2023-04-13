f186469fbc9987039e3f34a7bf2b2aee30a1a838
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-13 13:58:21 +0200

    Update App.tsx
    created inputs and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <Todolist />
+      <Todolist />
     </div>
   );
 }
 
 export default App;
-function Todolist () {
-  return <div>Todolist</div>
+function Todolist() {
+  return <div>
+    <h3>What to learn</h3>
+    <div>
+      <input/>
+      <button>+</button>
+    </div>
+    <ul>
+      <li><input type="checkbox" checked={true}/><span>CSS&HTML</span></li>
+      <li><input type="checkbox" checked={true}/><span>JS</span></li>
+      <li><input type="checkbox" checked={false}/><span>React</span></li>
+    </ul>
+    </div>;
 }
