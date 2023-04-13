1c0cca8e43f2421b3f0bf58e4ee38201c7c0ba4b
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-13 21:34:19 +0200

    Update App.tsx
    craeted tasks with initTasks array and setTasks function

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Todolist } from "./Todolist";
 
 function App() {
-  let tasks = [
+  let initTasks = [
     {id: 1, title: "CSS&HTML", isDone: true},
     {id: 2, title: "JS", isDone: true},
     {id: 3, title: "React", isDone: false},
     {id: 4, title: "Redux", isDone: false},
   ]
+
+  let arr = useState(initTasks);
+
+  let tasks = arr[0];
+  let setTasks = arr[1];
+
   function removeTask (id: number) {
     tasks = tasks.filter(t => t.id !== id)
   }
