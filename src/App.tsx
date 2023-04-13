8125bd5d32bea445a4b904e1f05d92ca57e10891
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-13 14:32:25 +0200

    created
    created Todolist file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Todolist from "./Todolist"
 
 function App() {
   return (
@@ -13,22 +14,4 @@ function App() {
 }
 
 export default App;
-function Todolist() {
-  return <div>
-    <h3>What to learn</h3>
-    <div>
-      <input/>
-      <button>+</button>
-    </div>
-    <ul>
-      <li><input type="checkbox" checked={true}/><span>CSS&HTML</span></li>
-      <li><input type="checkbox" checked={true}/><span>JS</span></li>
-      <li><input type="checkbox" checked={false}/><span>React</span></li>
-    </ul>
-    <div>
-    <button>All</button>
-    <button>Active</button>
-    <button>Completed</button>
-    </div>
-    </div>;
-}
+
