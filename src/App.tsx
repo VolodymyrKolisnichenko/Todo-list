b951e978081c855b81b88f1f6429694e6868c51f
Vova <90705859+anotherShine@users.noreply.github.com>
2023-04-13 15:43:52 +0200

    Update App.tsx
    created tasks and added to props

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,20 @@ import "./App.css";
 import { Todolist } from "./Todolist";
 
 function App() {
+  let task1 = [
+    {id: 1, title: "CSS&HTML", isDone: true},
+    {id: 2, title: "JS", isDone: true},
+    {id: 3, title: "React", isDone: false},
+  ]
+  let task2 = [
+    {id: 1, title: "Batman", isDone: true},
+    {id: 2, title: "Batwoman", isDone: false},
+    {id: 3, title: "Batchild", isDone: true},
+  ]
   return (
     <div className="App">
-      <Todolist title="What to learn" />
-      <Todolist title="What to learn" />
-      <Todolist title="What to learn" />
+      <Todolist title="What to learn" tasks={task1} />
+      <Todolist title="Movies" tasks={task2} />
     </div>
   );
 }
